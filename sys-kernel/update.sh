99185385f458e3887dfb3f746ab26b07e95f5a50
Sune Kloppenborg Jeppesen <skj@coming.dk>
2012-09-26 09:38:33 +0200

    Cleanout old stuff.

--- sys-kernel/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Downloading snapshot"
-wget -O - "http://git.overlays.gentoo.org/gitweb/?p=dev/anarchy.git;a=snapshot;h=a4869ab71bcaf4ffff201aec1db930d1dd821e66;sf=tgz" | tar xvvzf - --strip-components 1 -C hardened-sources
-
-echo "Updating digest"
-ebuild `ls -1 hardened-sources/*.ebuild | head -n 1` digest
